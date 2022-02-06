32b1590ed2f60de6fae7d70007bfd542e9c6aa8a
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-05 20:42:36 -0500

    Teste classe Cliente

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from pprint import pprint
+
 class Cliente:
     def __init__(self, nome, cpf, profissao):
         self.nome = nome
         self.cpf = cpf
         self.profissao = profissao
+
+#Testando a classe
+cliente = Cliente("Jhon Doe", "123.456.789-00", "Desenvolvedor")
+
+print(cliente.nome)
+print(cliente.cpf)
+print(cliente.profissao)
+
+print(cliente.__dict__)
+
+pprint(cliente.__dict__, width = 40)

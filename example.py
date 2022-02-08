6a10c6ab78716ea968ea4faf4c0b3c4408a1448f
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-07 21:13:38 -0500

    Capturando o tipo de erro e usando __class__.__bases__

--- example.py
+++ example.py
@@ -3,15 +3,19 @@ def dividir(dividendo, divisor):
     return dividendo / divisor
 
 def testa_divisao(divisor):
-    try:
         resultado = dividir (10, divisor)
         print(f"O resultado da divisao de 10 por {divisor} e {resultado}")
-    except ZeroDivisionError:
-        print("Erro de divisao por zero tratado")
+
 
 try:
     testa_divisao(0)
-except AttributeError:
+except AttributeError as E:
     print("Erro de atributo tratado")
+    print(E)
+    print(E.__class__.__bases__)
+except ZeroDivisionError as E:
+    print("Erro de divisao por zero tratado")
+    print(E)
+    print(E.__class__.__bases__)
 
 print("Programa encerrado")

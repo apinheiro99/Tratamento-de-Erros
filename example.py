4c0c15c1b2b7d85c5b7e6ea11f721029ffbbe966
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-07 20:37:52 -0500

    Correcao divisor

--- example.py
+++ example.py
@@ -4,6 +4,6 @@ def dividir(dividendo, divisor):
 def testa_divisao(divisor):
     try:
         resultado = dividir (10, divisor)
-        print(f"O resultado da divisao de 10 por {divisot} e {resultado}")
+        print(f"O resultado da divisao de 10 por {divisor} e {resultado}")
     except ZeroDivisionError:
         print("Erro de divisao por zero tratado")

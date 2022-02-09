f464680656fa476e5fa22b6c9d631fa069e48dcc
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-08 19:54:31 -0500

    Criando getters e setters

--- main.py
+++ main.py
@@ -11,12 +11,54 @@ class ContaCorrente:
     taxa_operacao = None
 
     def __init__(self, cliente, agencia, numero):
-        self.saldo =100
+        self.__saldo =100
         self.cliente = cliente
-        self.agencia = agencia
-        self.numero = numero
+        self.__agencia = agencia
+        self.__numero = numero
         ContaCorrente.total_contas_criadas += 1
         ContaCorrente.taxa_operacao = 30 / ContaCorrente.total_contas_criadas
+
+    @property
+    def agencia(self):
+        return self.__agencia
+
+    @agencia.setter
+    def agencia (self, value):
+        if not isinstance(value, int):
+            print("O atributo agencia deve ser inteiro")
+            return
+
+        if value <= 0:
+            print("O atributo agencia deve ser maior que zero")
+            return
+
+        self.__agencia = value
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    @numero.setter
+    def numero (self, value):
+        if not isinstance(value, int):
+            return
+        if value <= 0:
+            return
+
+        self.__numero = value
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @saldo.setter
+    def saldo (self, value):
+        if not isinstance(value, int):
+            return
+        if value <= 0:
+            return
+
+        self.__saldo = value
 
     def trasferir (self, valor, favorecido):
         favorecido.depositar (valor)
@@ -38,4 +80,10 @@ print(cliente.__dict__)
 
 pprint(cliente.__dict__, width = 40)
 
-conta_corrente = ContaCorrente(None, "00", "101")
+conta_corrente = ContaCorrente(None, "00A", "101")
+
+#Teste 02
+
+print("################################")
+conta_corrente.agencia = 0
+print(conta_corrente.agencia)

c2e1da22ad5ece047cb48160d05ac84b54d198f3
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-09 21:14:08 -0500

    Aprimoramento da classe SaldoInsuficienteError

--- exceptions.py
+++ exceptions.py
@@ -1,2 +1,8 @@
 class SaldoInsuficienteError(Exception):
-    pass
+    
+    def __init__(self, message="", saldo = None, valor = None, *args):
+        self.__saldo = saldo
+        self.__valor = valor
+        msg = "Saldo insuficiente para efetuar a transacao\n" \
+            f"Saldo atual: {self.__saldo}  Valor a ser sacado: {self.__valor}"
+        super(SaldoInsuficienteError, self).__init__(message or msg, *args)
